fbc20d9a51709805e625fb9307fa128b9b83a2cc
Rupesh Karale <rupeshkarale3245@gmail.com>
2023-08-25 10:00:32 +0530

    changes done

--- Config/db.js
+++ Config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 require('dotenv').config()
 mongoose.set("strictQuery", false);
-console.log(process.env.MONGO_URL)
 const connection = mongoose.connect(process.env.MONGO_URL)
+// const connection = mongoose.connect("mongodb://127.0.0.1:27017/banking");
+// const connection = mongoose.connect(`mongodb+srv://banking:${dbPassword}@cluster0.yzilnkf.mongodb.net/?retryWrites=true&w=majority`)
 module.exports = connection;
